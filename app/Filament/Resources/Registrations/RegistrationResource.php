18d2f2e70d4abadca37db99e9cec66e0c91eb403
Lucas Garbe <hi@lucasgarbe.de>
2025-08-23 14:47:23 +0200

    feat: add bare registration resource

---
+++ app/Filament/Resources/Registrations/RegistrationResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\Registrations;
+
+use App\Filament\Resources\Registrations\Pages\CreateRegistration;
+use App\Filament\Resources\Registrations\Pages\EditRegistration;
+use App\Filament\Resources\Registrations\Pages\ListRegistrations;
+use App\Filament\Resources\Registrations\Schemas\RegistrationForm;
+use App\Filament\Resources\Registrations\Tables\RegistrationsTable;
+use App\Models\Registration;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RegistrationResource extends Resource
+{
+    protected static ?string $model = Registration::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return RegistrationForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return RegistrationsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListRegistrations::route('/'),
+            'create' => CreateRegistration::route('/create'),
+            'edit' => EditRegistration::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
