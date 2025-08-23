c0916fc18861697fffc26f1554871f15d425892a
Lucas Garbe <hi@lucasgarbe.de>
2025-08-23 23:53:56 +0200

    feat: add custom theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->maxContentWidth(Width::Full)
             ->topNavigation()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
--- resources/views/filament/pages/manage-draw.blade.php
+++ resources/views/filament/pages/manage-draw.blade.php
@@ -1,32 +1,17 @@
 <x-filament-panels::page>
     <div class="space-y-6">
 
-        <!-- Action Buttons -->
-        <div class="bg-white shadow-sm ring-1 ring-gray-950/5 rounded-xl p-6">
-            <div class="flex sm:flex-row gap-4 justify-end">
-                <x-filament::button
-                    wire:click="submitDraw"
-                    color="success"
-                    size="lg"
-                    icon="heroicon-o-star"
-                    class="flex-1 justify-center">
-                    <span class="font-semibold">Execute Draw</span>
-                </x-filament::button>
-
-                <x-filament::button
-                    wire:click="getTrackStats"
-                    color="info"
-                    outlined
-                    size="lg"
-                    icon="heroicon-o-chart-bar"
-                    class="flex-1 justify-center">
-                    <span class="font-semibold">View Statistics</span>
-                </x-filament::button>
-            </div>
-        </div>
-
         <!-- Main Form -->
         {{ $this->form }}
+
+        <x-filament::button
+            wire:click="submitDraw"
+            size="lg"
+            color="danger"
+            icon="heroicon-o-arrow-path-rounded-square"
+            class="flex-1 justify-center">
+            <span class="font-semibold">Execute Draw</span>
+        </x-filament::button>
 
     </div>
 </x-filament-panels::page>
