cbb2f1654f0d1c57688b3ac3c54269931f749a14
Lucas Garbe <hi@lucasgarbe.de>
2026-04-20 16:32:10 +0200

    chore: template deployment config and seeders, update .gitignore

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'git@github.com:lucasgarbe/steppenreg.git');
+set('repository', 'git@github.com:your-org/steppenreg.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -16,7 +16,7 @@ add('writable_dirs', []);
 
 host('your-server.com')
     ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/steppenreg');
+    ->set('deploy_path', '~/your-app');
 
 // Hooks
 
